ffcb3f766e432a62c4ba40fb59a0bf9e06e4ba17
Karema <karemahamdy51@gmail.com>
2024-10-25 05:42:17 +0300

    feat: add supabase code

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getCabins } from '../services/apiCabins'
 
 export default function Cabin() {
+  useEffect(function() {
+    getCabins().then((data)=> console.log(data) )
+  },[])
   return (
     <div>
       Cabin
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://tlkyzpirifkgkmylfzwd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsa3l6cGlyaWZrZ2tteWxmendkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3OTE1MjEsImV4cCI6MjA0NTM2NzUyMX0.zofXQEGnsE6m7RCtbXV133_ASSiuakdiO0yCWs3miVU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
