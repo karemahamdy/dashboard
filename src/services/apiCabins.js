360281e222243112eb6fd970bf457e4244742546
Karema <karemahamdy51@gmail.com>
2024-10-26 02:21:56 +0300

    feat: fetch react query from api cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,8 +1,8 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  const { data, error } = await supabase.from("cabins").select("*");
-  console.log("test")
+  const { data, error } = await supabase.from("Cabins").select("*");
+  console.log(data)
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
