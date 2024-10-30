3a4a860d955015edde8499f8b915e05ba5acc0bd
Karema <karemahamdy51@gmail.com>
2024-10-30 03:11:04 +0300

    style: usetting ui

--- src/features/setting/SettingForm.jsx
+++ src/features/setting/SettingForm.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Input } from '../../components/ui/input';
+import { Label } from '../../components/ui/label';
+
 
 export default function SettingForm() {
+
   return (
-    <div>
+    <>
+    <div className="bg-white  rounded-md p-6">
+        <form>
       
+          <div className=" mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <Label htmlFor="maxbooking" className="sm:basis-40 ">Max Booking/nights</Label>
+            <Input
+        
+            id="maxbooking"
+            type="name"/>  
+         </div>
+
+          <div className=" mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <Label htmlFor="minbooking" className="sm:basis-40">Min Booking/nights</Label>
+            <Input
+        
+              id="minbooking"
+              type="name" />
+          </div>
+
+          <div className=" mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <Label htmlFor="maxbooking" className="sm:basis-40">Max guests/booking</Label>
+            <Input
+            
+              id="maxbooking"
+              type="name" />
+          </div>
+
+          <div className=" mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <Label htmlFor="breakfast-price" className="sm:basis-40">Breakfast Price</Label>
+            <Input
+            
+              id="breakfast-price"
+              type="number" />
+          </div>
+
+
+      </form>
     </div>
+    </>
   )
 }
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import SettingForm from "../features/setting/SettingForm";
+
 
 export default function Setting() {
   return (
-    <div>Setting</div>
+    <>
+    <h1 className="text-2xl font-bold px-4 pb-4">Update Hotel Setting</h1>
+    <SettingForm/>
+    </>
   )
 }
