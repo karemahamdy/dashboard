132f8a2452f0b188349dcbf341c4206e7935ea94
Karema <karemahamdy51@gmail.com>
2024-11-22 21:31:09 +0200

    feat: add dashboard layout

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -7,20 +7,20 @@ function Filter({ filterField, options }) {
   function handleClick(value) {
     searchParams.set(filterField, value);
     if (searchParams.get("page")) searchParams.set("page", 1);
-
     setSearchParams(searchParams);
   }
 
   return (
-    <div className="border border-gray-100 bg-red-100 shadow-sm rounded-sm p-1 flex gap-1">
+    <div className="flex gap-2 p-2 border border-grey-100 bg-grey-0 rounded-sm shadow-sm">
       {options.map((option) => (
         <button
           key={option.value}
           onClick={() => handleClick(option.value)}
-          className={`rounded-sm font-medium text-sm px-2 py-1 transition-colors
-            ${option.value === currentFilter ? 'bg-brand-600 text-brand-50' : 'bg-gray-50'}
-          `}
           disabled={option.value === currentFilter}
+          className={`px-3 py-2 text-sm font-medium transition rounded-sm ${option.value === currentFilter
+              ? "bg-brand-600 text-brand-50"
+              : "bg-grey-0 hover:bg-brand-600 hover:text-brand-50"
+            }`}
         >
           {option.label}
         </button>
