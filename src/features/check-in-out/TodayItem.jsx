c280d92f282ac79ffa5479407e75a990e25552b0
Karema <karemahamdy51@gmail.com>
2024-11-22 23:48:44 +0200

    fix: an err in layoit

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,42 +1,27 @@
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-// import Tag from "../../ui/Tag";
-// import { Flag } from "../../ui/Flag";
 import Button from "../../ui/Button";
 // import CheckoutButton from "./CheckoutButton";
-
-const StyledTodayItem = styled.li`
-  display: grid;
-  grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
-  gap: 1.2rem;
-  align-items: center;
-
-  font-size: 1.4rem;
-  padding: 0.8rem 0;
-  border-bottom: 1px solid var(--color-grey-100);
-
-  &:first-child {
-    border-top: 1px solid var(--color-grey-100);
-  }
-`;
-
-const Guest = styled.div`
-  font-weight: 500;
-`;
 
 function TodayItem({ activity }) {
   const { id, status, guests, numNights } = activity;
 
   return (
-    <StyledTodayItem>
-      {status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
-      {status === "checked-in" && <Tag type="blue">Departing</Tag>}
+    <li className="grid grid-cols-[9rem_2rem_1fr_7rem_9rem] gap-4 items-center text-sm py-2 border-b border-gray-100 first:border-t first:border-gray-100">
+      {/* Status */}
+      {status === "unconfirmed" && <span className="w-fit uppercase text-[1.1rem] font-semibold py-[0.4rem] px-[1.2rem] rounded-full" type="green">Arriving</span>}
+      {status === "checked-in" && <span className="w-fit uppercase text-[1.1rem] font-semibold py-[0.4rem] px-[1.2rem] rounded-full" type="blue">Departing</span>}
 
-      {/* <Flag src={guests.countryFlag} alt={`Flag of ${guests.country}`} /> */}
-      <Guest>{guests.fullName}</Guest>
+      {/* Flag */}
+      <img className="max-w-[2rem] rounded-[var(--border-radius-tiny)] block border border-gray-100" src={guests.countryFlag} alt={`Flag of ${guests.country}`} />
+
+      {/* Guest Name */}
+      <div className="font-medium">{guests.fullName}</div>
+
+      {/* Nights */}
       <div>{numNights} nights</div>
 
+      {/* Action buttons */}
       {status === "unconfirmed" && (
         <Button
           size="small"
@@ -48,7 +33,7 @@ function TodayItem({ activity }) {
         </Button>
       )}
       {/* {status === "checked-in" && <CheckoutButton bookingId={id} />} */}
-    </StyledTodayItem>
+    </li>
   );
 }
 
